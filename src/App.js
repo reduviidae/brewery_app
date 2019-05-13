b8f6efa858e73f26646d241b2592f53430c75fb5
reduviidae <wiljago@gmail.com>
2019-05-13 16:54:37 -0400

    fetching breweries

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import './App.scss';
 // components
 import NavBar from "./Components/NavBar";
 import MapContainer from "./Components/MapContainer";
+import BreweryList from "./Components/BreweryList"
 
 function App() {
   return (
     <div>
     <NavBar />
     <MapContainer />
+    <BreweryList />
     </div>
   );
 }
--- src/Components/BreweryList.js
+++ src/Components/BreweryList.js
@@ -1,1 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class BreweryList extends Component {
+  componentDidMount() {
+    this.fetchBreweries();
+  }
+
+  fetchBreweries() {
+    fetch(`https://api.openbrewerydb.org/breweries?by_state=new_york`, {
+      method: `GET`,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    })
+    .then(r => r.json())
+    .then(data => this.props.breweriesToState(data))
+}
+
+render () {
+  console.log(this.props.breweries);
+return (
+  <div>
+  THIS IS WHERE THE BREWERIES GO
+  </div>
+)
+}
+
+};
+
+const mapStateToProps = state => {
+  return {
+    breweries: state.breweries
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    breweriesToState: data => dispatch({type: "GET_BREWERIES", payload: data})
+  }
+  }
+
+  export default connect(mapStateToProps, mapDispatchToProps)(BreweryList);
